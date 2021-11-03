0a1feb8a726d536af9f75e96563b0f0adabbb6b3
alexanderoeka <alexanderoeka@gmail.com>
2021-11-03 13:21:31 +0400

    collectionss

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\BlogPost;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DiggingDeeperController extends Controller
 {
@@ -14,8 +15,8 @@ class DiggingDeeperController extends Controller
     {
         $result = [];
         $eloquentCollection = BlogPost::withTrashed()->get();
-        $collOne = BlogPost::withTrashed()->where('id',50)->get();
-        $collTwo = BlogPost::withTrashed()->where('id',104)->get();
+        $collOne = BlogPost::withTrashed()->where('id', 50)->get();
+        $collTwo = BlogPost::withTrashed()->where('id', 104)->get();
         $chto = $collTwo[0];
         $chto->title = 'AHAHAHAHH_NUNET_Blet';
         $chto->delete();
@@ -26,11 +27,21 @@ class DiggingDeeperController extends Controller
         $collection = collect($eloquentCollection->toArray());
 
 
-        dd(__METHOD__,
+        /*dd(__METHOD__,
             get_class($eloquentCollection),
             get_class($collection),
             $eloquentCollection,
             $collection
         );
+        */
+
+        //dd($collection);
+
+        $result['where']['data'] = $collection
+            ->where('category_id', 3)
+            ->where('deleted_at', '!=', null)
+            ->values()
+            ->keyBy('id');
+        dd($result);
     }
 }
